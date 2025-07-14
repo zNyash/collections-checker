7bd144224d7f87ed332f72abdd667ea64f3f5e11
Vitor Gabriel <37278974+zNyash@users.noreply.github.com>
2025-07-14 19:37:22 -0300

    Path finder is done.

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import chalk from "chalk";
+import { OsuPathFinder } from "./classes/OsuPathFinder";
+
+
+
+async function main() {
+  const osuPath = new OsuPathFinder().getOsuPath();
+}
+
+main();
