c09b328667140f8f9611a88faa311b2900ab9f8f
Vitor Gabriel <37278974+zNyash@users.noreply.github.com>
2025-12-04 06:55:21 -0300

    feat: add Logger class for structured logging and Constants for API URLs

---
+++ classes/Logger.ts
@@ -0,0 +1,65 @@
+// Logger.ts
+import chalk from "chalk";
+
+export type LogLevel = "debug" | "info" | "success" | "warn" | "error" | "fatal";
+
+const levelColors = {
+    debug: chalk.magenta,
+    info: chalk.blue,
+    success: chalk.green,
+    warn: chalk.yellow,
+    error: chalk.red,
+    fatal: chalk.bgRed.white.bold,
+};
+
+export class Logger {
+    private static instance: Logger;
+
+    private constructor() {}
+
+    static getInstance() {
+        if (!Logger.instance) {
+            Logger.instance = new Logger();
+        }
+        return Logger.instance;
+    }
+
+    private getTime() {
+        const now = new Date();
+        return now.toLocaleTimeString("en-US", {
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true,
+        });
+    }
+
+    private log(level: LogLevel, ...msg: any[]) {
+        const time = chalk.gray(this.getTime());
+        const tag = levelColors[level](`[${level.toUpperCase()}]`);
+        console.log(`${time} | ${tag} -`, ...msg);
+    }
+
+    debug(...msg: any[]) {
+        this.log("debug", ...msg);
+    }
+
+    info(...msg: any[]) {
+        this.log("info", ...msg);
+    }
+
+    success(...msg: any[]) {
+        this.log("success", ...msg);
+    }
+
+    warn(...msg: any[]) {
+        this.log("warn", ...msg);
+    }
+
+    error(...msg: any[]) {
+        this.log("error", ...msg);
+    }
+
+    fatal(...msg: any[]) {
+        this.log("fatal", ...msg);
+    }
+}
---
+++ utils/Constants.ts
@@ -0,0 +1,5 @@
+export enum Constant {
+    OsuMirrorApiUrl = "https://catboy.best/d/",
+    OsuMirrorRateLimitUrl = "https://catboy.best/api/ratelimits",
+    OsuMirrorAltApiUrl = "https://osu.direct/api/d/",
+}
